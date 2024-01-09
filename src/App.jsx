e2f75b542c8290e0d3340f2bd3b23f91edb4721c
meowy8 <mhoey8@hotmail.com>
2024-01-09 21:44:14 +0000

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import NavBar from "./components/NavBar"
+
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <div className="bg-[#15061c] min-w-screen min-h-screen text-white flex flex-col items-center">
+      <NavBar />
+      
+    </div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,11 @@
 const NavBar = () => {
   return (
-    <nav>
-      
+    <nav className="flex justify-between w-96">
+      <span>Pictiurlann</span>
+      <span>Articles</span>
+      <span>Profile</span>
     </nav>
   )
 }
+
+export default NavBar 
