18ae4f7869e3cf3aabb6e500ffab255e6404750b
meowy8 <mhoey8@hotmail.com>
2024-01-16 23:11:26 +0000

    created fetch for article data (works for article list)

--- src/components/article-components/ArticleContent.jsx
+++ src/components/article-components/ArticleContent.jsx
@@ -1,30 +1,70 @@
-import { useLocation } from "react-router-dom"
+import { useLocation, useParams } from "react-router-dom"
 import ArticleFooter from "./ArticleFooter"
 import ArticleHeaderImage from "./ArticleHeaderImage"
 import ArticleHeadingContainer from "./ArticleHeadingContainer"
 import ArticleMain from "./ArticleMain"
 import ArticlePreface from "./ArticlePreface"
+import { useEffect, useState } from "react"
 
 const ArticleContent = () => {
+  const [ articleContentData, setArticleContentData ] = useState(null)
+  const [ currentArticle, setCurrentArticle ] = useState(null)
   const location = useLocation()
-  const { src, title, published, preface, author, mainText, id } = location.state || {}
+  const { src, title, published, preface, author, mainText, } = location.state || {}
+  const { id } = useParams()
+  console.log('id from params: ' + id)
 
+  useEffect(() => {
+    fetch('/src/components/articleData.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json()
+    })
+    .then(data => {
+      setArticleContentData(data)
+    })
+    .catch(error => {
+      console.error('Error: ', error)
+    })
+  }, [])
+
+  useEffect(() => {
+    const currentArticleSearch = (id) => {
+      if (articleContentData) {
+        const correctArticle = articleContentData.find(article => article.id == id)
+        if (correctArticle) {
+          setCurrentArticle(correctArticle)
+        } else console.log('no article found')
+      }
+    }
+    
+    currentArticleSearch(id)
+  }, [id, articleContentData])
+
+  console.log('current article: ' + currentArticle)
 
   return (
+    <>
+   {currentArticle ? 
     <div className="flex flex-col gap-3 bg-[#5e2424] p-4">
       <ArticleHeadingContainer
-        title={title}
-        author={author}
-        id={id}
-        src={src}
-        published={published}
-        mainText={mainText}
-        preface={preface}/>
-      <ArticlePreface preface={preface}/>
-      <ArticleHeaderImage src={src}/>
-      <ArticleMain mainText={mainText}/>
-      <ArticleFooter published={published}/>
+        title={currentArticle.title}
+        author={currentArticle.author}
+        id={currentArticle.id}
+        src={currentArticle.src}
+        published={currentArticle.published}
+        mainText={currentArticle.mainText}
+        preface={currentArticle.preface}
+      />
+      <ArticlePreface preface={currentArticle.preface}/>
+      <ArticleHeaderImage src={currentArticle.src}/>
+      <ArticleMain mainText={currentArticle.mainText}/>
+      <ArticleFooter published={currentArticle.published}/>
     </div>
+    : <p>error</p>}
+    </>
   )
 }
 
--- src/components/article-components/ArticleList.jsx
+++ src/components/article-components/ArticleList.jsx
@@ -1,38 +1,30 @@
 import PageHeading from "../PageHeading"
 import ArticleSearch from "./ArticleSearch"
 import ArticlePreviewCard from "./ArticlePreviewCard"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const ArticlesList = () => {
-  const articleData = [
-    { title: 'Cinematic Poetry: Unraveling the Subtle Brilliance of Hong Sang-soo',
-      id: 1,
-      author: 'John Doe',
-      preface: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada justo nec quam fermentum, ac laoreet urna euismod. Vestibulum at leo vel libero malesuada feugiat. Aliquam erat volutpat.',
-      src: '/media/Novelists-Film-Header.webp',
-      mainText: 'Proin ac augue vel elit cursus efficitur. Suspendisse ac sapien vel purus blandit cursus. Fusce dignissim, justo eget vulputate mattis, ex quam aliquam quam, sit amet tincidunt justo mauris ac purus. Ut quis erat a arcu ultricies rhoncus. Integer vel lectus vel nisl efficitur scelerisque. Curabitur fringilla libero et erat tincidunt, in egestas leo volutpat. Sed vitae enim vel justo vehicula interdum. Curabitur ultrices, velit nec pharetra commodo, felis turpis fringilla nunc, id euismod odio elit vitae libero. Vivamus auctor gravida ante eu efficitur. Curabitur at tortor et neque congue suscipit. Nullam feugiat risus vel urna dapibus, eu lacinia orci vulputate. \n\n Quisque in massa nec mi dignissim volutpat. Morbi euismod, felis at fermentum fermentum, mauris justo dictum nisi, id fringilla libero dui ut felis. Vivamus ullamcorper, sem ac congue vestibulum, nulla quam viverra turpis, in rhoncus mi libero nec elit. Praesent eu arcu nec justo tristique facilisis. Suspendisse id metus in ligula bibendum tristique. Sed dapibus metus eu arcu congue, vitae bibendum orci finibus. Aenean sit amet metus quis orci sodales vestibulum. Integer eu justo in elit sodales tincidunt id eu dui. Vivamus non fringilla leo. Sed rhoncus velit vel nisl fermentum, eu blandit orci dapibus. Integer ac sapien vel quam volutpat pharetra. Suspendisse potenti. Integer interdum ullamcorper purus, vel luctus purus malesuada eu. Nunc aliquet velit vel leo accumsan cursus. Etiam hendrerit quam id mauris cursus, at tincidunt justo fringilla.',
-      published: '2022-01-01'
-    },
-    { title: 'Between the Lines: Jean-Luc Godard\'s Art of Subtext in Cinema',
-      id: 2,
-      author: 'Mary Jane',
-      preface: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada justo nec quam fermentum, ac laoreet urna euismod. Vestibulum at leo vel libero malesuada feugiat. Aliquam erat volutpat.',
-      src: '/media/Vivre-Sa-Vie-Header.jpg',
-      mainText: 'Proin ac augue vel elit cursus efficitur. Suspendisse ac sapien vel purus blandit cursus. Fusce dignissim, justo eget vulputate mattis, ex quam aliquam quam, sit amet tincidunt justo mauris ac purus. Ut quis erat a arcu ultricies rhoncus. Integer vel lectus vel nisl efficitur scelerisque. Curabitur fringilla libero et erat tincidunt, in egestas leo volutpat. Sed vitae enim vel justo vehicula interdum. Curabitur ultrices, velit nec pharetra commodo, felis turpis fringilla nunc, id euismod odio elit vitae libero. Vivamus auctor gravida ante eu efficitur. Curabitur at tortor et neque congue suscipit. Nullam feugiat risus vel urna dapibus, eu lacinia orci vulputate. \n\n Quisque in massa nec mi dignissim volutpat. Morbi euismod, felis at fermentum fermentum, mauris justo dictum nisi, id fringilla libero dui ut felis. Vivamus ullamcorper, sem ac congue vestibulum, nulla quam viverra turpis, in rhoncus mi libero nec elit. Praesent eu arcu nec justo tristique facilisis. Suspendisse id metus in ligula bibendum tristique. Sed dapibus metus eu arcu congue, vitae bibendum orci finibus. Aenean sit amet metus quis orci sodales vestibulum. Integer eu justo in elit sodales tincidunt id eu dui. Vivamus non fringilla leo. Sed rhoncus velit vel nisl fermentum, eu blandit orci dapibus. Integer ac sapien vel quam volutpat pharetra. Suspendisse potenti. Integer interdum ullamcorper purus, vel luctus purus malesuada eu. Nunc aliquet velit vel leo accumsan cursus. Etiam hendrerit quam id mauris cursus, at tincidunt justo fringilla.',
-      published: '2023-01-01'
-    },
-    { title: 'Michelangelo Antonioni\'s Landscapes of Emptiness: Unraveling the Aesthetics of Alienation',
-      id: 3,
-      author: 'John Doe',
-      preface: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada justo nec quam fermentum, ac laoreet urna euismod. Vestibulum at leo vel libero malesuada feugiat. Aliquam erat volutpat.',
-      src: '/media/Red-Desert-Header.jpeg',
-      mainText: 'Proin ac augue vel elit cursus efficitur. Suspendisse ac sapien vel purus blandit cursus. Fusce dignissim, justo eget vulputate mattis, ex quam aliquam quam, sit amet tincidunt justo mauris ac purus. Ut quis erat a arcu ultricies rhoncus. Integer vel lectus vel nisl efficitur scelerisque. Curabitur fringilla libero et erat tincidunt, in egestas leo volutpat. Sed vitae enim vel justo vehicula interdum. Curabitur ultrices, velit nec pharetra commodo, felis turpis fringilla nunc, id euismod odio elit vitae libero. Vivamus auctor gravida ante eu efficitur. Curabitur at tortor et neque congue suscipit. Nullam feugiat risus vel urna dapibus, eu lacinia orci vulputate. \n\n Quisque in massa nec mi dignissim volutpat. Morbi euismod, felis at fermentum fermentum, mauris justo dictum nisi, id fringilla libero dui ut felis. Vivamus ullamcorper, sem ac congue vestibulum, nulla quam viverra turpis, in rhoncus mi libero nec elit. Praesent eu arcu nec justo tristique facilisis. Suspendisse id metus in ligula bibendum tristique. Sed dapibus metus eu arcu congue, vitae bibendum orci finibus. Aenean sit amet metus quis orci sodales vestibulum. Integer eu justo in elit sodales tincidunt id eu dui. Vivamus non fringilla leo. Sed rhoncus velit vel nisl fermentum, eu blandit orci dapibus. Integer ac sapien vel quam volutpat pharetra. Suspendisse potenti. Integer interdum ullamcorper purus, vel luctus purus malesuada eu. Nunc aliquet velit vel leo accumsan cursus. Etiam hendrerit quam id mauris cursus, at tincidunt justo fringilla.',
-      published: '2020-01-01'
-    }
-  ]
-  
   const [ search, setSearch ] = useState('')
-  const [ filteredList, setFilteredList ] = useState(articleData)
+  const [ filteredList, setFilteredList ] = useState(null)
+
+ useEffect(() => {
+    fetch('/src/components/articleData.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json()
+    })
+    .then(data => {
+      setFilteredList(data)
+    })
+    .catch(error => {
+      console.error('Error: ', error)
+    })
+  }, [])
+
+  
+
 
   const handleSortAsc = (selection) => {
     if (selection === 'asc') {
@@ -45,7 +37,7 @@ const ArticlesList = () => {
   }
 
   const handleSearch = (searchValue) => {
-    const newList = articleData.filter(article => {
+    const newList = filteredList.filter(article => {
       return search.toLowerCase() === '' ? article : article.title.toLowerCase().includes(search)
     })
     setFilteredList(newList)
@@ -57,7 +49,7 @@ const ArticlesList = () => {
       <PageHeading>Articles</PageHeading>
       <ArticleSearch search={search} setSearch={setSearch} handleSearch={handleSearch} handleSortAsc={handleSortAsc}/>
       <ul className="flex flex-col gap-4">
-        {filteredList.map(article => {
+        {filteredList && filteredList.map(article => {
           return (
           <li key={article.id}>
             <ArticlePreviewCard 
